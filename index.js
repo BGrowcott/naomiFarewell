dfb60878d171d171f38f9f7a249507a3842a4a8e
bgrowcott <bgrowcott@ucb.ac.uk>
2026-05-01 16:13:42 +0100

    added more fun

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const orbitContainer = document.querySelector('.orbit-item-container');
+const centerImage = document.querySelector('.center-image');
 const containerSpinButton = document.querySelector('#containerSpinButton');
 const orbitSpinButton = document.querySelector('#orbitSpinButton');
 const orbitItems = document.querySelectorAll('.orbit-item');
@@ -6,7 +7,18 @@ const orbitItems = document.querySelectorAll('.orbit-item');
 let centralSpinSpeed = 15;
 let orbitSpinSpeed = 15;
 
+let centralClicks = 0;
+let orbitClicks = 0;
+
 const increaseCentralSpin = () => {
+    centralClicks++;
+
+    centerImage.style.width = `${centerImage.width + centralClicks}px`
+    centerImage.style.height = `${centerImage.width + centralClicks}px`
+
+    if (centralClicks % 10 === 0) {
+        fireConfetti();
+    }
 
     containerSpinButton.innerHTML = "Again!"
     if (centralSpinSpeed > 2) {
@@ -15,6 +27,7 @@ const increaseCentralSpin = () => {
         containerSpinButton.innerHTML = "More!"
         centralSpinSpeed = centralSpinSpeed - 0.1
     } else {
+        fireConfetti();
         containerSpinButton.innerHTML = "Yeea!"
     }  
 
@@ -22,7 +35,12 @@ const increaseCentralSpin = () => {
 };
 
 const increaseOrbitSpin = () => {
-    
+    orbitClicks++;
+
+    if (orbitClicks % 10 === 0) {
+        fireConfetti();
+    }
+
     orbitSpinButton.innerHTML = "Again!"
     if (orbitSpinSpeed > 2) {
         orbitSpinSpeed = orbitSpinSpeed - 2
@@ -30,6 +48,7 @@ const increaseOrbitSpin = () => {
         orbitSpinButton.innerHTML = "Again I said!"
         orbitSpinSpeed = orbitSpinSpeed - 0.1
     } else {
+        fireConfetti();
         orbitSpinButton.innerHTML = "boiiii!!!"
     }  
 
@@ -40,3 +59,31 @@ const increaseOrbitSpin = () => {
 
 containerSpinButton.addEventListener("click", increaseCentralSpin)
 orbitSpinButton.addEventListener("click", increaseOrbitSpin)
+
+const fireConfetti = () => {
+    const duration = 1200;
+    const end = Date.now() + duration;
+
+    (function frame() {
+        const edge = Math.floor(Math.random() * 4);
+
+        let origin;
+
+        switch (edge) {
+            case 0: origin = { x: Math.random(), y: 0 }; break;
+            case 1: origin = { x: Math.random(), y: 1 }; break;
+            case 2: origin = { x: 0, y: Math.random() }; break;
+            case 3: origin = { x: 1, y: Math.random() }; break;
+        }
+
+        confetti({
+            particleCount: 12,
+            spread: 120,
+            origin
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    })();
+};
